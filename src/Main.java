30513bf0cf4e6c5b0ec939cd98ba625ece209974
amalware <150959885+amalware@users.noreply.github.com>
2024-04-18 11:59:02 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,123 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
+
+    public static Scanner scanner = new Scanner(System.in); //to input stuff
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        ArrayList<String> tasks = new ArrayList<String>();
+
+
+
+
+        System.out.println("Please choose an option:\n(1) Add a task. \n(2) Remove a task. \n(3) Update a task. \n(4) List all tasks. \n(0) Exit.");
+        int selection;
+        try {
+            selection = scanner.nextInt();
+            scanner.nextLine(); //Eat enter
+        } catch(Exception e){
+            System.out.println("Please enter a number between 0 and 4.");
+            selection = scanner.nextInt();
+            scanner.nextLine(); //Eat enter
         }
+
+
+        while (selection != 0){ //Continue loop until user exits
+
+
+
+
+            switch (selection) { //Caters to each user selection
+                case 1: //Add task
+                    addTask(tasks);
+                    break;
+                case 2: //Remove task
+                    removeTask(tasks);
+
+
+                    break;
+                case 3: //update task
+                    updateTask(tasks);
+
+
+                    break;
+                case 4: //List all tasks
+                    System.out.println("Your current list of tasks is:");
+                    System.out.println(tasks);
+                    break;
+            }
+            System.out.println("Please choose an option:\n(1) Add a task. \n(2) Remove a task. \n(3) Update a task. \n(4) List all tasks. \n(0) Exit.");
+            try {
+                selection = scanner.nextInt();
+                scanner.nextLine(); //Eat enter
+            } catch(Exception e){
+                System.out.println("Please enter a number between 0 and 4.");
+                selection = scanner.nextInt();
+                scanner.nextLine(); //Eat enter
+            }
+            //Continue or break the loop
+        }; //exit the loop
+
+
+        System.out.println("Goodbye!");//end program
     }
+    public static ArrayList addTask(ArrayList a){
+        System.out.println("Title for your task: ");
+        String title = scanner.nextLine();
+        System.out.println("Description for your task: ");
+        String description = scanner.nextLine();
+        System.out.println("Priorty for your task (0-5): ");
+
+        int priority; //HERE THE ERROR IS HERE!!!!
+        try {
+            priority = scanner.nextInt();
+            scanner.nextLine(); //Eat enter
+        }catch(Exception e){
+            System.out.println("Please enter a number 0-5");
+            priority = scanner.nextInt();
+            scanner.nextLine(); //Eat enter
+        }
+        CreateTask task = new CreateTask(title, description, priority);
+        a.add(task);
+        Collections.sort(a);
+        System.out.println("Your new list of tasks is:");
+        System.out.println(a);
+        return a;
+    }
+    public static ArrayList removeTask(ArrayList a){
+        System.out.println("Which task would you like to remove? (Please name the index, starting from 0)");
+        System.out.println(a);
+        scanner.nextLine(); //Prevents an "eating an enter" error...
+        int b = scanner.nextInt();
+        a.remove(b);
+        System.out.println("Your new list of tasks is:");
+        System.out.println(a);
+        return a;
+    }
+
+
+    public static ArrayList updateTask(ArrayList a){
+        System.out.println("Which task would you like to update? (Please name the index, starting from 0)");
+        System.out.println(a);
+        scanner.nextLine(); //Prevents an "eating an enter" error...
+        int b = scanner.nextInt(); //get index
+        System.out.println("What would you like to update it to?");
+        scanner.nextLine();
+        String c = scanner.nextLine(); //get value
+        a.set(b, c);
+        System.out.println("Your new list of tasks is:");
+        System.out.println(a);
+        return a;
+    }
+
+
+
+
+
+
 }
+
+
+
